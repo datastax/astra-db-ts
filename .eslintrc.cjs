ac543e57be81798cc256ad6ee7ee735aa5183e9e
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2023-11-05 15:47:50 -0800

    Ignore dist dir when running linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /* eslint-env node */
 module.exports = {
+  ignorePatterns: ["dist"],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier', 'sort-destructure-keys'],
