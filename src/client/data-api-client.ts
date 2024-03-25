b7794f622f37a886603c79974e16a2417acc7847
toptobes <kavinpg@gmail.com>
2024-03-25 00:16:52 -0500

    Documented Data API class

--- src/client/data-api-client.ts
+++ src/client/data-api-client.ts
@@ -2,9 +2,41 @@ import { Db, mkDb } from '@/src/data-api/db';
 import { AstraAdmin, mkAdmin } from '@/src/devops/astra-admin';
 import { AdminSpawnOptions, DbSpawnOptions, RootClientOptions, RootClientOptsWithToken } from '@/src/client/types';
 
+/**
+ * The main entrypoint into working with the Data API. It sits at the top of the
+ * [conceptual hierarchy](https://github.com/datastax/astra-db-ts/tree/signature-cleanup?tab=readme-ov-file#abstraction-diagram)
+ * of the SDK.
+ *
+ * The client takes in a default token, which can be overridden by a stronger/weaker token when spawning a new
+ * {@link Db} or {@link AstraAdmin} instance.
+ *
+ * It also takes in a set of default options (see {@link RootClientOptions}) that may also be overridden as necessary.
+ *
+ * @example
+ * ```typescript
+ * const client = new DataAPIClient('AstraCS:...');
+ *
+ * const db1 = client.db('https://<db_id>-<region>.apps.astra.datastax.com');
+ * const db2 = client.db('my-database', 'us-east1');
+ *
+ * const coll = await db1.collection('my-collection');
+ *
+ * const admin1 = client.admin();
+ * const admin2 = client.admin({ adminToken: '<stronger_token>' });
+ *
+ * console.log(await coll.insertOne({ name: 'Lordi' }));
+ * console.log(await admin1.listDatabases());
+ * ```
+ */
 export class DataApiClient {
   readonly #options: RootClientOptsWithToken;
 
+  /**
+   * Constructs a new instance of the {@link DataApiClient}.
+   *
+   * @param token - The default token to use when spawning new instances of {@link Db} or {@link AstraAdmin}.
+   * @param options - The default options to use when spawning new instances of {@link Db} or {@link AstraAdmin}.
+   */
   constructor(token: string, options?: RootClientOptions) {
     this.#options = {
       ...options,
@@ -19,14 +51,94 @@ export class DataApiClient {
     };
   }
 
+  /**
+   * Spawns a new {@link Db} instance using a direct endpoint and given options.
+   *
+   * This endpoint should include the protocol and the hostname, but not the path. It's typically in the form of
+   * `https://<db_id>-<region>.apps.astra.datastax.com`, but it can be used with DSE or any other Data-API-compatible
+   * endpoint.
+   *
+   * The given options will override any default options set when creating the {@link DataApiClient} through
+   * a deep merge (i.e. unset properties in the options object will just default to the default options).
+   *
+   * Note that this does not perform any IO or validation on if the endpoint is valid or not. It's up to the user to
+   * ensure that the endpoint is correct. If you want to create an actual database, see {@link AstraAdmin.createDatabase}
+   * instead.
+   *
+   * @example
+   * ```typescript
+   * const db1 = client.db('https://<db_id>-<region>.apps.astra.datastax.com');
+   *
+   * const db2 = client.db('https://<db_id>-<region>.apps.astra.datastax.com', {
+   *   namespace: 'my-namespace',
+   *   useHttp2: false,
+   * });
+   * ```
+   *
+   * @param endpoint - The direct endpoint to use.
+   * @param options - Any options to override the default options set when creating the {@link DataApiClient}.
+   *
+   * @returns A new {@link Db} instance.
+   */
   db(endpoint: string, options?: DbSpawnOptions): Db;
 
+  /**
+   * Spawns a new {@link Db} instance using a direct endpoint and given options.
+   *
+   * This overload is purely for user convenience, but it **only supports using Astra as the underlying database**. For
+   * DSE or any other Data-API-compatible endpoint, use the other overload instead.
+   *
+   * The given options will override any default options set when creating the {@link DataApiClient} through
+   * a deep merge (i.e. unset properties in the options object will just default to the default options).
+   *
+   * Note that this does not perform any IO or validation on if the endpoint is valid or not. It's up to the user to
+   * ensure that the endpoint is correct. If you want to create an actual database, see {@link AstraAdmin.createDatabase}
+   * instead.
+   *
+   * @example
+   * ```typescript
+   * const db1 = client.db('a6a1d8d6-31bc-4af8-be57-377566f345bf', 'us-east1');
+   *
+   * const db2 = client.db('a6a1d8d6-31bc-4af8-be57-377566f345bf', 'us-east1', {
+   *   namespace: 'my-namespace',
+   *   useHttp2: false,
+   * });
+   * ```
+   *
+   * @param id - The database ID to use.
+   * @param region - The region to use.
+   * @param options - Any options to override the default options set when creating the {@link DataApiClient}.
+   *
+   * @returns A new {@link Db} instance.
+   */
   db(id: string, region: string, options?: DbSpawnOptions): Db;
 
   db(endpointOrId: string, regionOrOptions?: string | DbSpawnOptions, maybeOptions?: DbSpawnOptions): Db {
     return mkDb(this.#options, endpointOrId, regionOrOptions, maybeOptions);
   }
 
+  /**
+   * Spawns a new {@link AstraAdmin} instance using the given options to work with the DevOps API (for admin
+   * work such as creating/managing databases).
+   *
+   * **NB. This method is only available for Astra databases.**
+   *
+   * The given options will override any default options set when creating the {@link DataApiClient} through
+   * a deep merge (i.e. unset properties in the options object will just default to the default options).
+   *
+   * @example
+   * ```typescript
+   * const admin1 = client.admin();
+   * const admin2 = client.admin({ adminToken: '<stronger_token>' });
+   *
+   * const dbs = await admin1.listDatabases();
+   * console.log(dbs);
+   * ```
+   *
+   * @param options - Any options to override the default options set when creating the {@link DataApiClient}.
+   *
+   * @returns A new {@link AstraAdmin} instance.
+   */
   admin(options?: AdminSpawnOptions): AstraAdmin {
     return mkAdmin(this.#options, options);
   }
--- src/client/types.ts
+++ src/client/types.ts
@@ -1,28 +1,197 @@
 import { Caller } from '@/src/api';
 
+/**
+ * The default options for the {@link DataApiClient}. The Data API & DevOps specific options may be overridden
+ * when spawning a new instance of their respective classes.
+ */
 export interface RootClientOptions {
+  /**
+   * A winston log level (`'error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly'`).
+   *
+   * Defaults to `process.env.NODE_ENV === 'production' ? 'error' : 'info'`.
+   *
+   * @default process.env.NODE_ENV === 'production' ? 'error' : 'info'
+   */
   logLevel?: string,
+  /**
+   * The caller information to send with requests, of the form `[name, version?]`, or an array of such.
+   *
+   * The caller information is used to identify the client making requests to the server.
+   *
+   * It will be sent in the headers of the request as such:
+   * ```
+   * User-Agent: ...<name>/<version> astra-db-ts/<version>
+   * ```
+   *
+   * If no caller information is provided, the client will simply be identified as `astra-db-ts/<version>`.
+   *
+   * **NB. If providing an array of callers, they should be ordered from most important to least important.**
+   *
+   * @example
+   * ```typescript
+   * // 'my-app/1.0.0 astra-db-ts/1.0.0'
+   * const client1 = new DataApiClient('AstraCS:...', {
+   *   caller: ['my-app', '1.0.0'],
+   * });
+   *
+   * // 'my-app/1.0.0 my-other-app astra-db-ts/1.0.0'
+   * const client2 = new DataApiClient('AstraCS:...', {
+   *   caller: [['my-app', '1.0.0'], ['my-other-app']],
+   * });
+   * ```
+   */
   caller?: Caller | Caller[],
+  /**
+   * The default options when spawning a {@link Db} instance.
+   */
   dataApiOptions?: DbSpawnOptions,
+  /**
+   * The default options when spawning an {@link AstraAdmin} instance.
+   */
   devopsOptions?: AdminSpawnOptions,
 }
 
+/**
+ * The options available spawning a new {@link Db} instance.
+ *
+ * If any of these options are not provided, the client will use the default options provided by the {@link DataApiClient}.
+ */
+export interface DbSpawnOptions {
+  /**
+   * The namespace (aka keyspace) to use for the database.
+   *
+   * Defaults to `'default_keyspace'`. if never provided. However, if it was provided when creating the
+   * {@link DataApiClient}, it will default to that value instead.
+   *
+   * Every db method will use this namespace as the default namespace, but they all allow you to override it
+   * in their options.
+   *
+   * @example
+   * ```typescript
+   * const client = new DataApiClient('AstraCS:...');
+   *
+   * // Using 'default_keyspace' as the namespace
+   * const db1 = client.db('https://<db_id>-<region>.apps.astra.datastax.com');
+   *
+   * // Using 'my_namespace' as the namespace
+   * const db2 = client.db('https://<db_id>-<region>.apps.astra.datastax.com', {
+   *   namespace: 'my_keyspace',
+   * });
+   *
+   * // Finds 'my_collection' in 'default_keyspace'
+   * const coll1 = db1.collection('my_collection');
+   *
+   * // Finds 'my_collection' in 'my_namespace'
+   * const coll2 = db1.collection('my_collection');
+   *
+   * // Finds 'my_collection' in 'other_keyspace'
+   * const coll3 = db1.collection('my_collection', { namespace: 'other_keyspace' });
+   * ```
+   *
+   * @default 'default_keyspace'
+   */
+  namespace?: string,
+  /**
+   * The access token for the Data API, typically of the format `'AstraCS:...'`.
+   *
+   * If never provided, this will default to the token provided when creating the {@link DataApiClient}.
+   *
+   * @example
+   * ```typescript
+   * const client = new DataApiClient('strong-token');
+   *
+   * // Using 'strong-token' as the token
+   * const db1 = client.db('https://<db_id>-<region>.apps.astra.datastax.com');
+   *
+   * // Using 'weaker-token' instead of 'strong-token'
+   * const db2 = client.db('https://<db_id>-<region>.apps.astra.datastax.com', {
+   *   token: 'weaker-token',
+   * });
+   * ```
+   */
+  token?: string,
+  /**
+   * Whether to use HTTP/2 for requests.
+   *
+   * Both versions are typically interchangeable, but HTTP2 is generally recommended for better performance. However,
+   * some errors may differ between the two versions, due to different underlying implementations.
+   *
+   * Defaults to `true` if never provided. However, if it was provided when creating the {@link DataApiClient}, it will
+   * default to that value instead.
+   *
+   * @default true
+   */
+  useHttp2?: boolean,
+  /**
+   * Whether to log skipped options (logs as 'warn')
+   *
+   * Logs when a command is executed with options that are not supported by the client
+   *
+   * Defaults to `false`.
+   *
+   * @example
+   * ```typescript
+   * // Logs a warning 'findOne' does not support option 'unknownOption'
+   * await findOne({}, { unknownOption: 'value' });
+   * ```
+   *
+   * @default false
+   */
+  logSkippedOptions?: boolean,
+  /**
+   * The path to the Data API, which is going to be `api/json/v1` for all Astra instances. However, it may vary
+   * if you're using a different Data API-compatible endpoint.
+   *
+   * Defaults to `'api/json/v1'` if never provided. However, if it was provided when creating the {@link DataApiClient},
+   * it will default to that value instead.
+   *
+   * @default 'api/json/v1'
+   */
+  dataApiPath?: string,
+}
+
+/**
+ * The options available spawning a new {@link AstraAdmin} instance.
+ *
+ * **Note that this is only available when using Astra as the underlying database.**
+ *
+ * If any of these options are not provided, the client will use the default options provided by the {@link DataApiClient}.
+ */
+export interface AdminSpawnOptions {
+  /**
+   * The access token for the DevOps API, typically of the format `'AstraCS:...'`.
+   *
+   * If never provided, this will default to the token provided when creating the {@link DataApiClient}.
+   *
+   * May be useful for if you want to use a stronger token for the DevOps API than the Data API.
+   *
+   * @example
+   * ```typescript
+   * const client = new Data('weak-token');
+   *
+   * // Using 'weak-token' as the token
+   * const db = client.db();
+   *
+   * // Using 'strong-token' instead of 'weak-token'
+   * const admin = client.admin({ adminToken: 'strong-token' });
+   * ```
+   */
+  adminToken?: string,
+  /**
+   * The base URL for the devops API, which is typically always going to be the following:
+   * ```
+   * https://api.astra.datastax.com/v2
+   * ```
+   */
+  endpointUrl?: string,
+}
+
+/**
+ * @internal
+ */
 export interface RootClientOptsWithToken {
   logLevel?: string,
   caller?: Caller | Caller[],
   dataApiOptions: DbSpawnOptions & { token: string },
   devopsOptions: AdminSpawnOptions & { adminToken: string },
-}
-
-export interface DbSpawnOptions {
-  token?: string,
-  useHttp2?: boolean,
-  namespace?: string,
-  logSkippedOptions?: boolean,
-  dataApiPath?: string,
-}
-
-export interface AdminSpawnOptions {
-  adminToken?: string,
-  endpointUrl?: string,
 }
