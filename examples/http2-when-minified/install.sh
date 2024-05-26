56429436a9754cd6f21bd55f0d54aa813afbf140
toptobes <kavinpg@gmail.com>
2024-05-26 14:14:01 -0500

    updated example deps

--- examples/http2-when-minified/install.sh
+++
@@ -1,6 +0,0 @@
-cd ~/work/astra-db-ts || exit
-npm run build
-npm pack
-cd ~/work/astra-db-ts/examples/http2-when-minified || exit
-npm i ~/work/astra-db-ts/datastax-astra-db-*.tgz
-rm ~/work/astra-db-ts/datastax-astra-db-*.tgz
