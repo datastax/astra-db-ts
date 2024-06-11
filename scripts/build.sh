af418c35ad3fbf8f31e3b0de97488f6978f97876
toptobes <kavinpg@gmail.com>
2024-06-11 12:39:35 -0500

    add missing license bumf to rollup .d.ts file

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,6 +18,9 @@ npm run api-extractor && rm -r ./temp
 # Uses a more succinct licence notice + removes block comments (the rollup .d.ts file already contains the ts-doc)
 find ./dist -type f -name '*.js' -exec node scripts/reduce-comments.js {} \;
 
+# Adds the missing license notice to the rollup .d.ts
+node scripts/add-license-bumf.js dist/astra-db-ts.d.ts
+
 # Removes all .d.ts files except the main rollup .d.ts
 cd dist || return 1
 find . -type f -name '*.d.ts' ! -name 'astra-db-ts.d.ts' -exec rm {} +
