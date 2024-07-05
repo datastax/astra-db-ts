0a2727c07652b9e66564df1117bc2d258c307335
toptobes <kavinpg@gmail.com>
2024-07-04 21:16:28 -0500

    couple tiny fixes

--- scripts/list-embedding-providers.sh
+++ scripts/list-embedding-providers.sh
@@ -1,17 +1,8 @@
+# Errors on using unbound variables
+set -u
+
 # Properly sources the .env file to bring env variables into scope
 eval "$(tr -d '\r' < .env)"
-
-# Sanity check
-if [ -z "${APPLICATION_URI}" ]; then
-  echo "Error: APPLICATION_URI is not set."
-  exit 1
-fi
-
-# Sanity check
-if [ -z "${APPLICATION_TOKEN}" ]; then
-  echo "Error: APPLICATION_TOKEN is not set."
-  exit 1
-fi
 
 # jq strips away the irrelevant fields we don't care about
 curl -sL "${APPLICATION_URI}/api/json/v1" \
