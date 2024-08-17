c78c8f07d1372070c55f45a3383f872203323e89
toptobes <kavinpg@gmail.com>
2024-08-16 23:21:15 -0500

    nix + direnv support

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "astra-db-ts dev shell dependencies";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = [ pkgs.nodejs_20 ]; };
+      });
+}
