b79177562aa95a8aa64e36d2beb0804e9ca39a39
toptobes <kavinpg@gmail.com>
2024-10-05 15:04:28 -0500

    update build report

--- src/documents/datatypes/inet-address.ts
+++ src/documents/datatypes/inet-address.ts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import ipRegex from 'ip-regex';
-
 export class InetAddress {
   readonly #address: string;
   #version: 4 | 6 | undefined;
@@ -59,23 +57,48 @@ export class InetAddress {
 const IPv4Lengths = { max: 15, min: 7 };
 const IPv6Lengths = { max: 45, min: 2 };
 
+// =====================================================================================================================
+// From https://github.com/sindresorhus/ip-regex/blob/main/index.js
+// Was getting errors trying to import it while as a dependency, so just decided not to deal with it for the time being
+const v4 = '(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}';
+
+const v6segment = '[a-fA-F\\d]{1,4}';
+
+const v6 = `
+(?:
+(?:${v6segment}:){7}(?:${v6segment}|:)|                                    // 1:2:3:4:5:6:7::  1:2:3:4:5:6:7:8
+(?:${v6segment}:){6}(?:${v4}|:${v6segment}|:)|                             // 1:2:3:4:5:6::    1:2:3:4:5:6::8   1:2:3:4:5:6::8  1:2:3:4:5:6::1.2.3.4
+(?:${v6segment}:){5}(?::${v4}|(?::${v6segment}){1,2}|:)|                   // 1:2:3:4:5::      1:2:3:4:5::7:8   1:2:3:4:5::8    1:2:3:4:5::7:1.2.3.4
+(?:${v6segment}:){4}(?:(?::${v6segment}){0,1}:${v4}|(?::${v6segment}){1,3}|:)| // 1:2:3:4::        1:2:3:4::6:7:8   1:2:3:4::8      1:2:3:4::6:7:1.2.3.4
+(?:${v6segment}:){3}(?:(?::${v6segment}){0,2}:${v4}|(?::${v6segment}){1,4}|:)| // 1:2:3::          1:2:3::5:6:7:8   1:2:3::8        1:2:3::5:6:7:1.2.3.4
+(?:${v6segment}:){2}(?:(?::${v6segment}){0,3}:${v4}|(?::${v6segment}){1,5}|:)| // 1:2::            1:2::4:5:6:7:8   1:2::8          1:2::4:5:6:7:1.2.3.4
+(?:${v6segment}:){1}(?:(?::${v6segment}){0,4}:${v4}|(?::${v6segment}){1,6}|:)| // 1::              1::3:4:5:6:7:8   1::8            1::3:4:5:6:7:1.2.3.4
+(?::(?:(?::${v6segment}){0,5}:${v4}|(?::${v6segment}){1,7}|:))             // ::2:3:4:5:6:7:8  ::2:3:4:5:6:7:8  ::8             ::1.2.3.4
+)(?:%[0-9a-zA-Z]{1,})?                                             // %eth0            %1
+`.replace(/\s*\/\/.*$/gm, '').replace(/\n/g, '').trim();
+
+const IPRegex = new RegExp(`(?:^${v4}$)|^${v6}$`);
+const IPv4Regex = new RegExp(`^${v4}$`);
+const IPv6Regex = new RegExp(`^${v6}$`);
+// =====================================================================================================================
+
 function isValidIP(raw: string) {
   if (raw.length < IPv4Lengths.min || IPv6Lengths.max < raw.length) {
     return false;
   }
-  return ipRegex({ exact: true }).test(raw);
+  return IPRegex.test(raw);
 }
 
 function isIPv6(raw: string) {
   if (raw.length < IPv6Lengths.min || IPv6Lengths.max < raw.length) {
     return false;
   }
-  return ipRegex.v6({ exact: true }).test(raw);
+  return IPv4Regex.test(raw);
 }
 
 function isIPv4(raw: string) {
   if (raw.length < IPv4Lengths.min || IPv4Lengths.max < raw.length) {
     return false;
   }
-  return ipRegex.v6({ exact: true }).test(raw);
+  return IPv6Regex.test(raw);
 }
