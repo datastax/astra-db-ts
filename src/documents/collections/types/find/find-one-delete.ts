7a3d5c6dd6d60fd46682c1166533eb6f0b27c0a3
toptobes <kavinpg@gmail.com>
2024-11-19 22:33:02 +0530

    a

--- src/documents/collections/types/find/find-one-delete.ts
+++ src/documents/collections/types/find/find-one-delete.ts
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import type { Projection, Sort } from '@/src/documents';
-import type { WithTimeout } from '@/src/lib';
+import type { GenericFindOneAndDeleteOptions } from '@/src/documents';
 
 /**
  * Represents the options for the `findOneAndDelete` command.
@@ -26,51 +25,4 @@ import type { WithTimeout } from '@/src/lib';
  *
  * @public
  */
-export interface CollectionFindOneAndDeleteOptions extends WithTimeout<'generalMethodTimeoutMs'> {
-  /**
-   * The order in which to apply the update if the filter selects multiple documents.
-   *
-   * If multiple documents match the filter, only one will be updated.
-   *
-   * Defaults to `null`, where the order is not guaranteed.
-   * @defaultValue null
-   */
-  sort?: Sort,
-  /**
-   * Specifies which fields should be included/excluded in the returned documents.
-   *
-   * If not specified, all fields are included.
-   *
-   * When specifying a projection, it's the user's responsibility to handle the return type carefully, as the
-   * projection will, of course, affect the shape of the returned documents. It may be a good idea to cast
-   * the returned documents into a type that reflects the projection to avoid runtime errors.
-   *
-   * @example
-   * ```typescript
-   * interface User {
-   *   name: string;
-   *   age: number;
-   * }
-   *
-   * const collection = db.collection<User>('users');
-   *
-   * const doc = await collection.findOne({}, {
-   *   projection: {
-   *     _id: 0,
-   *     name: 1,
-   *   },
-   *   vector: [.12, .52, .32],
-   *   includeSimilarity: true,
-   * }) as { name: string, $similarity: number };
-   *
-   * // Ok
-   * console.log(doc.name);
-   * console.log(doc.$similarity);
-   *
-   * // Causes type error
-   * console.log(doc._id);
-   * console.log(doc.age);
-   * ```
-   */
-  projection?: Projection,
-}
+export type CollectionFindOneAndDeleteOptions = GenericFindOneAndDeleteOptions;
