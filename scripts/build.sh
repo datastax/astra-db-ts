115421c632717a1ddac2adb89e0d589c0625c9c7
toptobes <kavinpg@gmail.com>
2025-02-06 21:24:34 +0530

    clean up & speed up build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,48 +19,41 @@ done
 # Cleans the previous build
 rm -rf ./dist
 
-# Creates the version file
+# Creates the new version file
 node scripts/utils/build-version-file.cjs > src/version.ts
+
+transpile_project() {
+  npx tsc -p "etc/tsconfig.$1.json"
+  npx tsc-alias -p "etc/tsconfig.$1.json"
+  echo "{\"type\": \"$2\"}" > "dist/$1/package.json"
+}
 
 # Transpiles the project
 if [ "$light" = true ]; then
-  npx tsc -p etc/tsconfig.cjs.json || exit 10
-  npx tsc-alias -p etc/tsconfig.cjs.json
-  echo '{"type": "commonjs"}' > dist/cjs/package.json
+  transpile_project "cjs" "commonjs"
 else
-  npx tsc -p etc/tsconfig.esm.json || exit 20
-  npx tsc-alias -p etc/tsconfig.esm.json
-  echo '{"type": "module"}' > dist/esm/package.json
+  transpile_project "esm" "module" &
+  transpile_project "cjs" "commonjs" &
+  wait
 
-  npx tsc -p etc/tsconfig.cjs.json || exit 30
-  npx tsc-alias -p etc/tsconfig.cjs.json
-  echo '{"type": "commonjs"}' > dist/cjs/package.json
-fi
-
-# Replaces alias paths with relative paths (e.g. `@/src/version` -> `../../src/version`)
-
-if [ "$light" != true ]; then
   # Creates the rollup .d.ts, generates an API report in etc/, and cleans up any temp files
   npx api-extractor run -c ./api-extractor.jsonc --local
-
-  # Updates the API report if flag not set
   if [ "$update_report" = true ]; then
     mv -f ./temp/*.api.md ./etc/
   fi
   rm -r ./temp
 
-  # For some reason the rollup .d.ts has some random, unused imports that need to be removed
-  node scripts/utils/remove-bad-rollup-imports.cjs dist/astra-db-ts.d.ts
+  # Adds licence header to rollup .d.ts
+  # Also, for some reason, the rollup .d.ts has some random, unused imports that need to be removed
+  node scripts/utils/process-rollup.cjs dist/astra-db-ts.d.ts
 
   # Uses a more succinct licence notice + removes block comments (the rollup .d.ts file already contains the ts-doc)
-  find ./dist -type f -name '*.js' -exec node scripts/utils/reduce-comments.cjs {} \;
-
-  # Adds the missing license notice to the rollup .d.ts
-  node scripts/utils/add-license-bumf.cjs dist/astra-db-ts.d.ts
+  find ./dist -type f -name "*.js" -print0 | xargs -P 10 -0 -I {} node scripts/utils/reduce-comments.cjs {}
 
   # Protects against Symbol.asyncIterator not found
-  sed -i.bak -E '/^(\s+)\[Symbol\.asyncIterator\]/i\
-    // @ts-ignore-error - May or may not be found depending on TS version & esnext.disposable in lib' dist/astra-db-ts.d.ts
+  sed -i.bak '/^[[:space:]]*\[Symbol\.asyncIterator\]/i\
+    // @ts-ignore-error - May or may not be found depending on TS lib version
+  ' dist/astra-db-ts.d.ts
   rm dist/astra-db-ts.d.ts.bak
 
   # Delete the "empty" files where only types were declared
